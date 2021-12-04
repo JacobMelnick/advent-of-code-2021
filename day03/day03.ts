1fb365fd5d851cd98eae842d10e078be9edc6f3a
Jacob Melnick <jake.r.melnick@gmail.com>
2021-12-03 17:14:35 -0800

    part 1 day 03

--- day03/day03.ts
+++ day03/day03.ts
@@ -2,34 +2,39 @@ import fs from "fs";
 
 const fileString = fs.readFileSync(__dirname + "/day03.data.txt").toString();
 
-const data = fileString.split("\r\n");
+const data = fileString.split("\r\n").map((row: string) => {
+  return row.split('').map((bit: string) => parseInt(bit))
+});
 
-let gammaNumber = "";
-let epsilonNum = "";
-let iterator = 0;
 
-if (gammaNumber.length < 13) {
-  let gamma = [];
-  let epsilon = [];
-  for (let i = 0; i < data.length; i++) {
-    const curr = data[i];
-    if (curr[iterator] === "1") {
-      gamma.push(curr[iterator]);
-    } else {
-      epsilon.push(curr[iterator]);
+
+  const calculateGamma = (inputData: any) => {
+    let gamma = [];
+    let epsilon = [];
+    for (let i = 0; i < inputData[0].length; i++) {
+      let totalBits = {
+        ones: 0,
+        zeros: 0
+      }
+      for (let j = 0; j < inputData.length; j++) {
+        const current = inputData[j][i]
+        if (current === 1) {
+          totalBits.ones++;
+        } else {
+          totalBits.zeros++
+        }
+      }
+      if (totalBits.ones > totalBits.zeros) {
+          gamma.push(1)
+          epsilon.push(0)
+      } else {
+        gamma.push(0)
+        epsilon.push(1)
+      }
+      }
+      
+      return {gamma, epsilon}
     }
-  }
-  console.log(gamma, 'test');
-  
-  if (gamma.length > epsilon.length) {
-    gammaNumber += gamma[0];
-    epsilonNum += epsilon[0];
-    iterator++
-  } else if (gamma.length < epsilon.length) {
-    epsilonNum += epsilon[0];
-    gammaNumber += gamma[0];
-    iterator++
-  }
-}
 
-console.log(epsilonNum, gammaNumber, iterator);
+console.log(calculateGamma(data));
+// multiply gamma and epsilon
