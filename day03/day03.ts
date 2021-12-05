f52f36056444952c59ad664228302cbbd25ec407
Jacob Melnick <jake.r.melnick@gmail.com>
2021-12-04 16:05:33 -0800

    day03 complete

--- day03/day03.ts
+++ day03/day03.ts
@@ -6,47 +6,112 @@ const data = fileString.split("\r\n").map((row: string) => {
   return row.split("").map((bit: string) => parseInt(bit));
 });
 
-const oxygenArr = data;
-const scrubber = data;
+let oxygenArr = data;
+let scrubber = data;
 
-const calculateGamma = (inputData: any) => {
+const O2RateComparator = ({
+  zeroTotal,
+  oneTotal,
+}: {
+  zeroTotal: number;
+  oneTotal: number;
+}): number => {
+  if (zeroTotal > oneTotal) {
+    return 0;
+  } else if (zeroTotal < oneTotal) {
+    return 1;
+  } else {
+    return 1;
+  }
+};
 
+const C02RateComparator = ({
+  zeroTotal,
+  oneTotal,
+}: {
+  zeroTotal: number;
+  oneTotal: number;
+}): number => {
+  if (zeroTotal > oneTotal) {
+    return 1;
+  } else if (zeroTotal < oneTotal) {
+    return 0;
+  } else {
+    return 0;
+  }
+};
+
+const calculateGamma = (inputData: Array<Array<number>>) => {
   let gamma = [];
   let epsilon = [];
 
-  const findOxygen = (oxygenArr: any, scrubber: any) => {
-    if (oxygenArr.length === 1) {
-      return oxygenArr;
-    }
-
-    for (let i = 0; i < inputData[0].length; i++) {
-      let totalBits = {
-        ones: 0,
-        zeros: 0,
-      };
-      for (let j = 0; j < inputData.length; j++) {
-        const current = inputData[j][i];
-        if (current === 1) {
-          totalBits.ones++;
-        } else {
-          totalBits.zeros++;
-        }
-      }
-      if (totalBits.ones > totalBits.zeros) {
-        gamma.push(1);
-        epsilon.push(0);
+  for (let i = 0; i < inputData[0].length; i++) {
+    let totalBits = {
+      ones: 0,
+      zeros: 0,
+    };
+    let totalBitsForOxygen = {
+      ones: 0,
+      zeros: 0,
+    };
+    let totalBitsForScrubber = {
+      ones: 0,
+      zeros: 0,
+    };
+    for (let j = 0; j < inputData.length; j++) {
+      const current = inputData[j][i];
+      if (current === 1) {
+        totalBits.ones++;
       } else {
-        gamma.push(0);
-        epsilon.push(1);
+        totalBits.zeros++;
+      }
+    }
+    if (totalBits.ones > totalBits.zeros) {
+      gamma.push(1);
+      epsilon.push(0);
+    } else {
+      gamma.push(0);
+      epsilon.push(1);
+    }
+    for (let k = 0; k < oxygenArr.length; k++) {
+      let currOxy = oxygenArr[k][i];
+      if (currOxy === 1) {
+        totalBitsForOxygen.ones++;
+      } else {
+        totalBitsForOxygen.zeros++;
+      }
+    }
+    for (let l = 0; l < scrubber.length; l++) {
+      const currScrub = scrubber[l][i];
+      if (currScrub === 1) {
+        totalBitsForScrubber.ones++;
+      } else {
+        totalBitsForScrubber.zeros++;
       }
     }
 
-    let newArr = oxygenArr.filter((num: any, i: any) => num[i] === gamma[i])
-    let otherArr = oxygenArr.filter((num: any, i: any) => num[i] === scrubber[i])
+    if (oxygenArr.length !== 1) {
+      oxygenArr = oxygenArr.filter(
+        (num: any) =>
+          num[i] ===
+          O2RateComparator({
+            zeroTotal: totalBitsForOxygen.zeros,
+            oneTotal: totalBitsForOxygen.ones,
+          })
+      );
+    }
+    if (scrubber.length !== 1) {
+      scrubber = scrubber.filter(
+        (num: any) =>
+          num[i] ===
+          C02RateComparator({
+            zeroTotal: totalBitsForScrubber.zeros,
+            oneTotal: totalBitsForScrubber.ones,
+          })
+      );
+    }
+  }
 
-  };
-
-  findOxygen(oxygenArr, scrubber);
   return { gamma, epsilon };
 };
 
@@ -54,3 +119,6 @@ const { gamma, epsilon } = calculateGamma(data);
 
 const gammaV = parseInt(gamma.join(""), 2);
 const eValue = parseInt(epsilon.join(""), 2);
+const scrubberVal = parseInt(oxygenArr[0].join(""), 2);
+const oxygenVal = parseInt(scrubber[0].join(""), 2);
+console.log(scrubberVal * oxygenVal);
