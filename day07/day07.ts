3b7fd108bf5aa6e7358fdfa8989be39f31d1730d
Jacob Melnick <jake.r.melnick@gmail.com>
2021-12-08 14:23:09 -0800

    day07 part two

--- day07/day07.ts
+++ day07/day07.ts
@@ -10,11 +10,27 @@ const crabs = fs
 // const fuel = (pos: any, steps: any) => {
 //     crabs.reduce((acc: any, curr: any) => acc + steps(Math.abs(pos - curr)), 0)
 // };
+// console.log(fuel(median, n => n)); 
 let fuel = 0;
 const median = crabs[Math.floor(crabs.length / 2)];
 for (let i = 0; i < crabs.length; i++) {
   fuel += Math.abs(crabs[i] - median);
 }
-console.log(fuel);
+// console.log(fuel);
 
-// console.log(fuel(median, n => n));
+
+//part 2
+
+let fuelExpo = [];
+
+for (let i = Math.min(...crabs); i <= Math.max(...crabs); i++) {
+  fuelExpo[i] = 0
+  for (let j = 0; j < crabs.length; j++) {
+    const currDif = Math.abs(crabs[j] - i);
+    fuelExpo[i] += currDif *((currDif + 1) / 2) 
+  }
+  
+}
+
+console.log(Math.min(...fuelExpo));
+
