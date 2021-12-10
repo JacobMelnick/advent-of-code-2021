13d37b8e9d63c0da2073a8408209ca7154e2f707
Jacob Melnick <jake.r.emelnick@gmail.com>
2021-12-10 10:52:47 -0800

    part 1 day 9

--- day09/day09.ts
+++ day09/day09.ts
@@ -1,37 +1,45 @@
 import fs from "fs";
 
-const smokeShows = fs
+const fileString = fs
   .readFileSync(__dirname + "/day09.data.txt")
   .toString()
-  .split("")
-  .map((row: string) => {
-    return row.split("").map((item: string) => parseInt(item));
-  });
 
-console.log(smokeShows);
-const directions = {
-  up: [0, 1],
-  down: [0, -1],
-  left: [-1, 0],
-  right: [0, 1],
-};
-let countOfHeights = [];
+  const smokeShows = fileString.split('\n').map(row => row.split('').map(curr => Number(curr)))
 
-for (let i = 0; i < smokeShows.length; i++) {
-  const height = smokeShows[i];
-  if (
-    height[directions.up] <= height &&
-    height[directions.right] <= height &&
-    height[directions.left] <= height &&
-    height[directions.down] <= height
-  ) {
-    countOfHeights.push(height);
+
+const isLowest = (data: any, x: any, y: any) => {
+  let adjacent = [];
+
+  const directions = {
+    up: y + 1,
+    down: y - 1 ,
+    left: x - 1,
+    right: x + 1,
+  };
+  
+  if (data[y][directions.left] !== undefined) {
+    adjacent.push(data[y][directions.left])
   }
+  if (data[y][directions.right] !== undefined) {
+    adjacent.push(data[y][directions.right])
+  }
+  if (data[directions.up] !== undefined) {
+    adjacent.push(data[directions.up][x])
+  }
+  if (data[directions.down] !== undefined) {
+    adjacent.push(data[directions.down][x])
+  }
+  
+  for (const point of adjacent) {
+    if (data[y][x] >= point) {
+      return false
+    }
+  }
+  return true
+  
 }
-const resultHeights = countOfHeights
-  .map((current: any) => {
-    current++;
-  })
-  .reduce((acc, curr: any) => acc + curr, 0);
+const part1 = smokeShows.reduce((outer: any, row, y) => outer + row
+.filter((num, x) => isLowest(smokeShows, x, y))
+.reduce((inner, number) => inner + number + 1, 0), 0)
 
-// console.log(resultHeights);
+console.log(part1);
